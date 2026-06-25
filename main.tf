9e66970fd5a1bf45d93b8d48e3e9cd551f8b31b5
Avi <avinashshetty2004@gmail.com>
2026-06-25 20:00:40 +0530

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "remotebackends3bucket"
+    key    = "avi/terraform.tfstate"
+    region = "ap-south-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
